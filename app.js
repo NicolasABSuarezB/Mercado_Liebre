0ce522373c909c058c8bed962ef508c575e09caf
Nicolas Suarez <nicosube2014@gmail.com>
2022-12-27 11:36:48 -0300

    Finalizacion TP08

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')))
 
-app.listen(port,() => console.log(`Se levantó con éxito el servidor en http://localhost:${port}`))
+app.listen(port, () => console.log(`Se levantó con éxito el servidor en http://localhost:${port}`))
